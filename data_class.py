e41f29f4143ab4d7e71d6d8759d29f83ec657880
Aditya Subrahmanya Bhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-08-29 21:05:35 +0530

    Create data_class.py

---
+++ data_class.py
@@ -0,0 +1,12 @@
+"""
+0 - letters
+1 - mostly forms , not sure though , need more data to confirm
+2 - short letters
+3 - letters again
+4 - posters/articles
+6 - research papers
+7 - descriptive/specification papers
+9 - newspaper articles
+14 - CVs
+15 - memorandum
+"""
