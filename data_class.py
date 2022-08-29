a226beecc94761865a5409e3f1e7de868ba36a76
Aditya Subrahmanya Bhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-08-29 21:16:04 +0530

    Update data_class.py

--- data_class.py
+++ data_class.py
@@ -1,8 +1,8 @@
 """
 0 - letters
-1 - mostly forms , not sure though , need more data to confirm
+1 - they seem to be contract forms
 2 - short letters
-3 - letters again
+3 - letters again ( contains letters and some posters )
 4 - posters/articles
 6 - research papers
 7 - descriptive/specification papers
